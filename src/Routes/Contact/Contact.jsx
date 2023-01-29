85e699746873a6e4bee0d1f2dbc4ccd72645f008
Rahul Kumar <rahul_kumar@applicatetechnology.com>
2023-01-30 00:24:47 +0530

    added formil and yup in form

--- src/Routes/Contact/Contact.jsx
+++ src/Routes/Contact/Contact.jsx
@@ -1,22 +1,64 @@
 import React, { useState } from 'react'
 import { FaLinkedinIn, FaGithub, FaRegThumbsUp, FaRegThumbsDown } from 'react-icons/fa'
 import { InputText} from "primereact/inputtext";
-import { Button } from 'primereact/button';
+import { Formik } from "formik";
+import * as Yup from 'yup';
+import axios from 'axios';
 import './Contact.css'
 const Contact = () => {
   const [likeState,setLikeState]=useState();
-  const [values,setValues]=useState({
-    guestName:undefined,
-    comment:undefined
-  })
-  const {guestName,comment}=values;
-  const handleSubmit=()=>{
+  const initialValues={
+    guestName:'',
+    comment:'',
+    email:'',
+  }
 
+  const schema = Yup.object().shape({
+    guestName:Yup.string().min(2,'Name is too short').max(100,'Name is too long').required('Name is required'),
+    email: Yup.string().email().required("Email is required"),
+    comment:Yup.string().min(2,'Comment is too short').max(1000,'Comment is too long'),
+  })
+
+
+  const submitForm=async (values)=>{
+   if(likeState === false || likeState===true){
+    const formData = new FormData();
+    formData.append('guestName',values.guestName);
+    formData.append('email',values.email);
+    formData.append('comment',values.comment);
+    formData.append('like',likeState);
+    try{
+      const resp = await axios.post('/review',formData);
+    }catch(err){
+      console.log(err)
+    }
+   }else{
+     console.log('Pls press the thumb button')
+   }
   }
   const handleLikeChange=(likeVal)=>{
     setLikeState(likeVal);
   }
+
+
   return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={schema}
+      onSubmit={submitForm}
+    >
+      {(formik) => {
+        const {
+          values,
+          handleChange,
+          handleSubmit,
+          errors,
+          touched,
+          handleBlur,
+          isValid,
+          dirty
+        } = formik;
+        return (
     <div className="contact_container">
       <div className="contact_header">
         <h1>Contact Details</h1>
@@ -44,21 +86,36 @@ const Contact = () => {
           <h3>Hey, there</h3>
           <p>If you like my website , so can you please like this and if you want you can send a compliment too !</p>
           <span className="p-float-label input-wrapper">
-          <InputText id="guestName" type="text" name="guestName" className="p-inputtext-lg textInput" value={guestName} onChange={(e)=>{setValues({...values,[e.target.name]:e.target.value})}}/>
+          <InputText id="guestName" type="text" name="guestName" className="p-inputtext-lg textInput" value={values.guestName}  onChange={handleChange} onBlur={handleBlur}/>
           <label htmlFor="guestName">Your Name</label>  
+          {errors.guestName && touched.guestName && (
+          <span className="error">{errors.guestName}</span>
+          )}
           </span>
+        <span className="p-float-label input-wrapper">
+          <InputText id="email" type="email"  name="email" className="p-inputtext-lg textInput"  value={values.email}  onChange={handleChange} onBlur={handleBlur}/>
+          <label htmlFor="email">Email</label> 
+          {errors.email && touched.email && (
+          <span className="error">{errors.email}</span>
+          )}
+        </span>
           <span className="p-float-label input-wrapper">
-          <InputText id="comment" type="text"  name="comment" className="p-inputtext-lg textInput"  value={comment} onChange={(e)=>{setValues({...values,[e.target.name]:e.target.value})}}/>
+          <InputText id="comment" type="text"  name="comment" className="p-inputtext-lg textInput"  value={values.comment}  onChange={handleChange} onBlur={handleBlur}/>
           <label htmlFor="comment">Comment</label>  
+          {errors.comment && touched.comment && (
+          <span className="error">{errors.comment}</span>
+          )}
         </span>
         <div className="like_button_container">
-          <span className={likeState===false ? 'greenClr':null}><FaRegThumbsDown  onClick={()=>{handleLikeChange(false)}}/> </span>
-          <span className={likeState===true ? 'greenClr':null}><FaRegThumbsUp  onClick={()=>{handleLikeChange(true)}}/></span>
+          <span className={likeState===false ? 'redClr':null}><FaRegThumbsDown className="like_icon"  onClick={()=>{handleLikeChange(false)}}/> </span>
+          <span className={likeState===true ? 'greenClr':null}><FaRegThumbsUp className="like_icon"  onClick={()=>{handleLikeChange(true)}}/></span>
         </div>
-          <button onClick={handleSubmit} className="submit_button">Send</button>
-        </div>
+          <button onClick={handleSubmit} className="submit_button"  disabled={!(dirty && isValid)}>Send</button>
+        </div> 
       </div>
     </div>
+        )}}
+    </Formik>
   )
 }
 
